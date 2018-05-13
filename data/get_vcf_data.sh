354fcf016a3e7bc54294f4e3c064d1fa19c9b063
Mohit Thakur <mohitmthakur@yahoo.com>
2018-05-13 19:00:49 -0400

    Added mkdir in script

--- data/get_vcf_data.sh
+++ data/get_vcf_data.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 # This script is expected to be run from the popgen folder.
 # This script will download very large amounts of data and will take a LONG time to run.
 
 URL="http://hgdownload.cse.ucsc.edu/gbdb/hg19/1000Genomes/phase3"
-
+mkdir -p data/vcf
 cd data/vcf
 for i in {1..22}
 do
